c9f18e5717f386b3c4ee8f9353b94bd67eb03c81
serena <serenaws@foxmail.com>
2015-04-14 15:31:47 +0800

    添加经典算法题：猴子吃桃和百钱买百鸡

---
+++ src/github/bubble/learn/practice/Chickens.java
@@ -0,0 +1,42 @@
+package github.bubble.learn.practice;
+
+/**
+ * Created by wangshuang on 2015/4/14.
+ *
+ * 5 pence a rooster, 3 pence a hen and chickens 3 only a penny,
+ * With 100 pence to buy one hundred chickens,
+ * the rooster, hen, the chicken must have,
+ * ask the rooster, hen, chicken how much to buy only just fill 100 pence.
+ */
+public abstract class Chickens {
+    public static int chickens(){
+        int number=0;
+        for(int x = 1; x < 20; x++){
+            for(int y = 1; y < 33; y++){
+                int z = 100 - x - y;
+                if((z % 3 == 0) && (x * 5 + y * 3 + z / 3 ==100)){
+                    System.out.println("公鸡："+x+"母鸡："+y+"小鸡："+z);
+                    number++;
+                }
+            }
+        }
+        return number;
+    }
+
+    /**
+     * https://docs.google.com/document/d/1Mj0PD2QYZ9XXW1AWABDrDRIR3dgTJp0uB87rBypuWtE/edit?usp=sharing
+     * @return
+     */
+    public static int bestChickes(){
+        int x,y,z,number=0;
+        for(int k=1;k<3;k++){
+            x=4*k;
+            y=25-7*k;
+            z=75+3*k;
+            System.out.println("公鸡：" + x + "母鸡：" + y + "小鸡：" + z);
+            number++;
+        }
+        return number;
+    }
+
+}
